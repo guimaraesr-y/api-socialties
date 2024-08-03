e0b4689e27ea626321d186b5a60ede9dd4d65067
Ryan Guimarães <guimaraesryan26@gmail.com>
2024-08-03 16:56:51 -0300

    feat: first commit

---
+++ src/main/java/br/com/socialties/SocialtiesApplication.java
@@ -0,0 +1,13 @@
+package br.com.socialties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialtiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialtiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/socialties/SocialtiesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.socialties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialtiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

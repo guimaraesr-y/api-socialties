e97d7a032095b1dae1fb4b14f2760a94c8e2ddf2
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-07-29 08:59:50 -0300

    Allow h2-console and remove storage initialization

--- src/main/java/br/com/socialties/SocialtiesApplication.java
+++ src/main/java/br/com/socialties/SocialtiesApplication.java
@@ -5,23 +5,16 @@ import br.com.socialties.domain.storage.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
+@ConfigurationPropertiesScan
 public class SocialtiesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialtiesApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-			storageService.deleteAll();
-			storageService.init();
-		};
 	}
 
 }
--- src/main/java/br/com/socialties/infra/security/SecurityConfig.java
+++ src/main/java/br/com/socialties/infra/security/SecurityConfig.java
@@ -33,10 +33,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .headers(frameOptions -> frameOptions.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
